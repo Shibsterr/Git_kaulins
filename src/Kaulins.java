b6648ae528e4f4ebd16a6b9601b922d91a99ae3f
Gutmanis <nauirssgg@gmail.com>
2023-03-21 09:09:50 +0200

    Veikti koda uzlabojumi

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -2,20 +2,21 @@ import java.util.Random;
 import java.util.Scanner;
 public class Kaulins {
 	
-	static void mestKaulinu(int reizes){
-		int skaitlis;
+	static int mestKaulinu(int reizes){
+		int skaitlis = 0;
 		Random rand = new Random();
 		for(int i=0; i<reizes; i++){
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
 		}
+		return skaitlis;
 	}
 	
 	public static void main(String[] args) {
-		int reizes = 0, izvele;
+		int reizes = 0, izvele, pedejais = 0;
 		Scanner scan = new Scanner(System.in);
 		do{
-		System.out.print("1-Mest kauliņu | 2-Apturēt");
+		System.out.print("1-Mest kauliņu | 2-Apskatit pēdejo | 3-Apturēt ");
 		izvele = scan.nextInt();
 		switch(izvele){
 		case 1:
@@ -23,9 +24,12 @@ public class Kaulins {
 				System.out.println("Cik reizes velies mest kauliņu?");
 				reizes = scan.nextInt();
 		}while(reizes<1);
-			mestKaulinu(reizes);
+		pedejais =	mestKaulinu(reizes);
 			break;
 		case 2:
+System.out.println("Pedejais uzmetais ir "+pedejais);
+			break;
+		case 3:
 			System.out.println("Programma apturēta!");
 			break;
 		}
