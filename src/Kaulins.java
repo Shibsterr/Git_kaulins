375ae73350378383d8939770fa802134a8808ecb
Kudums <kudumsendijs@gmail.com>
2023-03-21 09:50:04 +0200

    Veikti labojumi programma funcionējoša

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,55 +1,55 @@
-import java.awt.List;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 public class Kaulins {
 	
-	static int mestKaulinu(int reizes){
-		int skaitlis = 0, lielakais=0;
+	static int mestKaulinu(){
 		Random rand = new Random();
-		for(int i=0; i<reizes; i++){
-		skaitlis = rand.nextInt(6)+1;
-		System.out.println("Uzkrita skaitlis: "+skaitlis);
-		if(lielakais<skaitlis){
-			lielakais = skaitlis;
-		}
-		}
-		System.out.println("Lielakais skaitlis metienu piegājienā ir "+lielakais);
+		int skaitlis = rand.nextInt(6)+1;
 		return skaitlis;
 	} 
 	
 	public static void main(String[] args) {
-		int reizes = 0, izvele, pedejais = 0;
-		ArrayList<Integer> pedejie = new ArrayList<Integer>();
+		int reizes = 0, izvele, lielakais=0;
+		List<Integer> pedejie = new ArrayList<Integer>();
 		Scanner scan = new Scanner(System.in);
 		do{
-		System.out.print("1-Mest kauliņu | 2-Apskatit pēdejo | 3-Apturēt ");
+			
+		System.out.print("1-Mest kauliņu || 2-Apskatit pēdejo metienu || 3-Apskatīt lielāko pēdējā metienā	|| 4-Apturēt ");
 		izvele = scan.nextInt();
+		
 		switch(izvele){
 		case 1:
 			do{
 				System.out.println("Cik reizes velies mest kauliņu?");
 				reizes = scan.nextInt();
 		}while(reizes<1);
-		pedejais =	mestKaulinu(reizes);
+				
+			for(int i=0; i<reizes; i++){
+				pedejie.add(mestKaulinu());
+			}
 			break;
 		case 2:
-System.out.println("Pedejais uzmetais ir "+pedejais);
-	for(int i=0; i<pedejie.size(); i++){
-	pedejie.get(i);
-	}
+			System.out.println("Pedejais uzmetais ir: ");
+		for(Integer i : pedejie){
+				System.out.println(i+" ");
+		}
+		System.out.println();
 			break;
 		case 3:
-			int lielakais=0;
 			for(int i=0; i<pedejie.size(); i++){
-				lielakais = pedejie.get(i);
+				if(pedejie.get(i)>lielakais){
+					lielakais = pedejie.get(i);
+				}
 			}
+			System.out.println("Lielākais skaitlis pēdēja metienā ir: "+lielakais);
 			break;
 		case 4:
 			System.out.println("Programm apturēta!");
 			break;
 		}
-		}while(izvele!=2);
+		}while(izvele!=4);
 		scan.close();
 	}
 }
